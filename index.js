fc992ba842c510f8042ebf570c678a515ca860fa
alaaajjel123 <104193154+alaaajjel123@users.noreply.github.com>
2023-12-03 08:31:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ app.get('/', (request, response) => {
 })
 
 app.get('/products', db.getProducts)
-app.get('/products/:id', db.getProductById)
+/*app.get('/products/:id', db.getProductById)
 app.post('/products', db.createProduct)
 app.put('/products/:id', db.updateProduct)
-//app.delete('/products/:id', db.deleteProduct)
+//app.delete('/products/:id', db.deleteProduct)*/
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
